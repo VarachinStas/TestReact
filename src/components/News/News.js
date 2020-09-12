9ffaa0eec3ba5cd9eb6f2f24eadfd4f6e0e1d2b5
Varachin_Stas <varachin_stas@mail.ru>
2020-09-12 15:36:33 +0300

    added all other things

---
+++ src/components/News/News.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './News.css'
+
+const News = () => {
+    return (
+        <div>
+            news
+        </div>
+    )
+}
+
+export default News;
