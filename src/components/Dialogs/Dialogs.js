3106a5f3132803e78c51fae49dc9070120266485
VarachinStas <varachin_stas@mail.ru>
2020-09-14 15:19:54 +0300

    all refact Profile & dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -5,8 +5,8 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-    let dialogsElements = props.state.dialogsPage.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-    let messagesElements = props.state.dialogsPage.messages.map(m => <Message message={m.message} id={m.id}/>);
+    let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = props.state.messages.map(m => <Message message={m.message} id={m.id}/>);
 
     return (
         <div className={classes.dialogs}>
