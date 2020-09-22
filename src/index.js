1b073b37b45301cb42d80bd99956a14d0fe1c284
VarachinStas <varachin_stas@mail.ru>
2020-09-22 17:48:28 +0300

    rename state.js to store.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import * as serviceWorker from './serviceWorker';
-import store from "./redux/state";
+import store from "./redux/store";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
-let rerenderAll = (state) => {  //из state.js берем props
+let rerenderAll = (state) => {  //из store.js берем props
     ReactDOM.render(
         <BrowserRouter>
             <App state={state} dispatch={store.dispatch.bind(store)} store={store}/>
