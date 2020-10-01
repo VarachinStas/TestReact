d41cc1eab340d5362c787a29c5bd0ccffd857f90
VarachinStas <varachin_stas@mail.ru>
2020-10-01 17:34:12 +0300

    48 end

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -1,7 +1,7 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
 const SEND_MESSAGE = 'SEND_MESSAGE';
 
-let initialState ={
+let initialState = {
     dialogs: [
         {id: 1, name: 'Stas'},
         {id: 2, name: 'Batman'},
@@ -20,19 +20,20 @@ let initialState ={
     newMessageBody: ""
 }
 
-const dialogsReducer = (state=initialState, action) => {
-
-    let stateCopy = {...state}
-
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body
-            return state;
+            return {
+                ...state,
+                newMessageBody: action.body
+            }
         case SEND_MESSAGE:
             let body = state.newMessageBody
-            state.newMessageBody = ''
-            state.messages.push({id: 6, message: body})
-            return state;
+            return {
+                ...state,
+                messages: [...state.messages, {id: 6, message: body}],
+                newMessageBody: ''
+            }
         default:
             return state;
     }
